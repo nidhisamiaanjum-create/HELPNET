21b1a41086cd5511dee40f3342fb995ca256da89
SamiaNidhi <nidhisamiaanjum@gmail.com>
2026-09-10 03:01:04 +0600

    registration module done

---
+++ backend/apps/users/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "full_name",
+            "email",
+            "phone_number",
+            "password",
+            "role",
+            "location",
+        ]
+
+    def validate_phone_number(self, value):
+        if User.objects.filter(phone_number=value).exists():
+            raise serializers.ValidationError(
+                "This phone number is already registered."
+            )
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                "This email is already registered."
+            )
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+
+        user = User.objects.create_user(
+            password=password,
+            **validated_data
+        )
+
+        return user
