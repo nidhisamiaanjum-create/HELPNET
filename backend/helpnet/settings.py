47755da7a96b0e1e9fc74cfd1cd76bfd915cfcb2
SamiaNidhi <nidhisamiaanjum@gmail.com>
2026-09-22 22:35:34 +0600

    updated css

--- backend/helpnet/settings.py
+++ backend/helpnet/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'helpnet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.parent / 'frontend'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -148,7 +148,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR.parent / 'frontend',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
