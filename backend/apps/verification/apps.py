43df381eda087d36a956feec55a3ebb058d1822b
SamiaNidhi <nidhisamiaanjum@gmail.com>
2026-09-23 04:02:19 +0600

    fix verification

--- backend/apps/verification/apps.py
+++ backend/apps/verification/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
 
-
-
-
 class VerificationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-<<<<<<< HEAD
+
     name = "apps.verification"
-=======
+
     name = "apps.verification"
->>>>>>> 17a3ceefeeadf555b9ec9c6a57e3304a6137ed5d
+
--- backend/apps/verification/views.py
+++ backend/apps/verification/views.py
@@ -1,13 +1,10 @@
 
 from django.shortcuts import render
 
-
-<<<<<<< HEAD
 def nid_verification_page(request):
     return render(request, "apps.verification/nid-verification.html")
-=======
 
 
 def nid_verification_page(request):
     return render(request, "apps.verification/nid-verification.html")
->>>>>>> 17a3ceefeeadf555b9ec9c6a57e3304a6137ed5d
+
